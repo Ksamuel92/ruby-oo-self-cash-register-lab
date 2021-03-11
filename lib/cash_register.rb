02df5b18850b08aa425d798fdd6ce557aef5d490
Kyle Samuel <Ksamuel92@gmail.com>
2021-03-11 12:54:28 -0500

    commit tree d02c7e37426e6b4549cfd1019ed0146702c9e9cf

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,33 @@
+require "pry"
+class CashRegister
+    attr_accessor :total, :discount, :items, :last_item_price
+
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+        @items = []
+    end
+    
+    #instance methods
+    def add_item (item, price, quantity = 1)
+        @items.fill(item, @items.size, quantity) #.fill takes three arguments, 1st the item to be "pushed", the index of where the item is to be pushed, and then the quantity of the item
+        @total += price * quantity
+        @last_item_price = price * quantity
+        @total
+        # binding.pry
+    end
+
+    def apply_discount
+        if self.discount == 0
+        return "There is no discount to apply."
+        else
+        @total = @total - (@total * @discount / 100)
+        "After the discount, the total comes to $#{@total}."
+        end
+    end
+
+    def void_last_transaction
+        @total -= @last_item_price
+    end
+end
 
